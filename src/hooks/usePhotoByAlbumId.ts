a446640492cb32ff45ab996ef1ecc4864ccaa66a
Rasmus Lempinen <ralem@Rasmuss-MBP.local>
2022-08-21 19:12:07 +0300

    Removed unused file. Added id to album query key.

--- src/hooks/usePhotoByAlbumId.ts
+++ src/hooks/usePhotoByAlbumId.ts
@@ -3,9 +3,13 @@ import { getPhotosByAlbumId } from "services/photoService";
 
 /** Fetch all photos in a specific album */
 const usePhotosByAlbumId = (albumId: string) => {
-  return useQuery<Api.Photo[], Error>(["photos-by-album-id"], () => getPhotosByAlbumId(albumId), {
-    enabled: true,
-  });
+  return useQuery<Api.Photo[], Error>(
+    [`photos-by-album-id-${albumId}`],
+    () => getPhotosByAlbumId(albumId),
+    {
+      enabled: true,
+    },
+  );
 };
 
 export default usePhotosByAlbumId;
